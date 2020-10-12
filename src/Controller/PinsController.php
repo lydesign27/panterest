0303323ad88b63f5649faa70f9a2153486241e34
Leidiane Yvanoff <leidianeyvanoff@mbp-de-leidiane.home>
2020-10-12 17:33:02 +0200

    Refactoring of create/edit by using PinType and make use of PUT as method for edit

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,11 +34,8 @@ class PinsController extends AbstractController
 
         $pin = new Pin;
 
-        $form = $this->createFormBuilder($pin)
-             ->add('title', TextType::class)
-             ->add('description', TextareaType::class)
-             ->getForm()
-        ;
+        $form = $this->createForm(PinType::class, $pin);
+
 
         $form->handleRequest($request);
 
@@ -64,14 +62,15 @@ class PinsController extends AbstractController
     }
 
      /**
-     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "POST"})
+     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "PUT"})
      */
     public function edit(Request $request, Pin $pin, EntityManagerInterface $em): Response
     {
-        $form = $this->createFormBuilder($pin)
-             ->add('title', TextType::class)
-             ->add('description', TextareaType::class)
-             ->getForm()
+      
+        $form = $this->createForm(PinType::class, $pin, [
+            'method' => 'PUT'
+        ])
+        
         ;
 
         $form->handleRequest($request);
