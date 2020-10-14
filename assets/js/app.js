288d9ff0cb3bfdd4537ed1da14e4a1c02196126d
Leidiane Yvanoff <leidianeyvanoff@mbp-de-leidiane.home>
2020-10-14 18:15:20 +0200

    Make of use webpack encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import '../scss/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+import 'bootstrap';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+
